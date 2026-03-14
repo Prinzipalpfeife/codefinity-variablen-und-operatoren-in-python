6df382d718d236f3e9dde71c49d70c9f9f961ee1
Prinzipalpfeife <>
2026-03-14 14:25:31 +0000

    Herausforderung: Umrechnung von Kilometern in Meilen

--- abschnitt/herausforderung_umrechnung_von_kilometern_in_meilen/main.py
+++ abschnitt/herausforderung_umrechnung_von_kilometern_in_meilen/main.py
@@ -2,10 +2,10 @@
 distance_in_kilometers = 4677.4
 
 # Convert distance to miles and store in `distance_in_miles`
-___ = ___
+distance_in_miles = distance_in_kilometers / 1.609
 
 # Convert variable type to int and store in `int_distance_in_miles`
-___ = ___
+int_distance_in_miles = int(distance_in_miles)
 
 # Testing
 print("San Francisco - New York distance (float): ", distance_in_miles, "mi")
