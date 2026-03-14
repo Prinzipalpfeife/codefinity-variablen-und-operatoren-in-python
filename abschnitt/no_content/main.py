afa3f0977ef77500f922b39ac29081ffd965e173
Prinzipalpfeife <>
2026-03-14 10:46:54 +0000

    No content

---
+++ abschnitt/no_content/main.py
@@ -0,0 +1,13 @@
+# Number of classes and students per class
+classes = 6
+students = 45
+
+# Calculate total number of students
+___ = ___
+
+# Calculate total number of classes
+___ = ___
+
+# Testing
+print("Number of students:", total_students)
+print("Number of groups:", total_classes)
